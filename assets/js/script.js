c4dbdcee39a1f821dd2d352fb1d19c4d53c90467
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-03-12 23:54:08 -0500

    functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,67 @@
 // Assignment code here
-//length of password
-var min_length = 8; //Minimum length of password
-var max_length = 128; //Maximum length of the password
-var p_length = window.prompt("What is the length of the password?"); //input value by user
 
 //pasword properties
 const upper = "abcdefghijklmnopqrstuvwxyz"
 const lower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
+//length of password
+var min_length = 8; //Minimum length of password
+var max_length = 128; //Maximum length of the password
 
-//Criterias for Password
-// var upper_case = window.prompt("Click 1 if you want Upper Case letters. Click 0 if you don't.")
-// var lower_case = window.prompt("Click 1 if you want Lower Case letters. Click 0 if you don't.")
-// var numeric = window.prompt("Click 1 if you want Numeric Numbers. Click 0 if you don't.")
-// var special_c = window.prompt("Click 1 if you want Special Characters letters. Click 0 if you don't.")
 
-// (if upper_case ===1)
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // Generate Password
-function generatePassword () {
+function generatePassword(upper_case, lower_case, numeric, special_c) {
 
   var characters = "";
-//checking for upper case requirement
-if (upper_case===1) {
-  characters=upper
+  //checking for upper case requirement
+  if (upper_case === "1") {
+    characters = upper
+  }
+  //checking for lower case requirement
+  if (lower_case === "1") {
+    characters = lower + characters
+  }
+  //checking for numbers requirement
+  if (numeric === "1") {
+    characters = numbers + characters
+  }
+  //checking for special characters requirement
+  if (special_c === "1") {
+    characters = symbols + characters
+  }
+  var password = "";
+  for (let i = 0; i < p_length; i++) {
+    var characterNumber = Math.floor(Math.random() * characters.length); //picking a number
+    var singleCharacter = characters[characterNumber] //picking a password property for a random number
+    password = password + singleCharacter //creating password
+  }
+  return password
 }
-//checking for lower case requirement
-if (lower_case===1) {
-  characters=lower+characters
-}
-//checking for numbers requirement
-if (numeric===1) {
-  characters=numbers+characters
-}
-//checking for special characters requirement
-if (spe===1) {
-  special_c=special_c+characters
-}
-
-for (let i = 0; i < p_length; i++) {
-  Math.floor(Math.random() * 11);
-}
-
-}
-
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var p_length = window.prompt("What is the length of the password?"); //input value by user
+
+  if (p_length < 8 || p_length > 128) {
+    alert("Please select a length between 8 to 128.")
+    return
+  }
+  
+  //Criterias for Password
+  var upper_case = window.prompt("Click 1 if you want Upper Case letters. Click 0 if you don't.")
+  if (upper_case !=="1"|| upper_case !=="0") {
+    alert("Please select a valid response.")
+    return
+  }
+  var lower_case = window.prompt("Click 1 if you want Lower Case letters. Click 0 if you don't.")
+  var numeric = window.prompt("Click 1 if you want Numeric Numbers. Click 0 if you don't.")
+  var special_c = window.prompt("Click 1 if you want Special Characters letters. Click 0 if you don't.")
+  alert("Please click the Generate button for your preferred password; reload the page to pick a password with different length")
+  var password = generatePassword(upper_case, lower_case, numeric, special_c);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -58,3 +70,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+}
